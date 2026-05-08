dfdba80ca8d8c1a31ae4782dc59ab0523ff67d04
mcborax4-lgtm <mcborax4@gmail.com>
2026-05-08 15:08:22 +0300

    move media files to media folder

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
